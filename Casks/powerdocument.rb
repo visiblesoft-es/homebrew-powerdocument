8ce37b2686edbb85e5467e47496a3df78795b25f
Jose L. Garcia <78475380+jlgarcia-vs@users.noreply.github.com>
2026-03-23 12:30:08 +0100

    Add PowerDocument cask formula

---
+++ Casks/powerdocument.rb
@@ -0,0 +1,29 @@
+# Homebrew Cask formula for PowerDocument
+# This file goes in the GitHub repo: visiblesoft-es/homebrew-powerdocument
+# in the path: Casks/powerdocument.rb
+#
+# Users install with:
+#   brew tap visiblesoft/powerdocument
+#   brew install --cask powerdocument
+#
+# Update checklist when releasing a new version:
+#   1. Update `version` to the new version number
+#   2. Run: shasum -a 256 PowerDocument_<version>_aarch64.dmg
+#   3. Update `sha256` with the new hash
+
+cask "powerdocument" do
+  version "0.1.0"
+  sha256 "REPLACE_WITH_ACTUAL_SHA256"
+
+  url "https://github.com/visiblesoft-es/PowerDocument/releases/download/v#{version}/PowerDocument_#{version}_aarch64.dmg"
+  name "PowerDocument"
+  desc "Lightweight Markdown document reader"
+  homepage "https://github.com/visiblesoft-es/PowerDocument"
+
+  app "PowerDocument.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.visiblesoft.powerdocument",
+    "~/Library/Caches/com.visiblesoft.powerdocument",
+  ]
+end
